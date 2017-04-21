e6aede6b2cd9a1835b9d0f2a3b171653f1037e94
Ethan Brown <ewb@ewb.io>
2017-04-21 18:31:53 +0200

    added minified js

--- public/javascripts/jquery.emailhide.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  var spt = $('span.mailme');
-  var at = / foo /;
-  var dot = / bar /g;
-  var addr = $(spt).text().replace(at,"@").replace(dot,".");
-  $(spt).before('<a href="mailto:'+addr+'" title="Email me or something">Email me</a> ')
-  .hover(function(){window.status="Click this link or something.";}, function(){window.status="";});
-  $(spt).remove();
-});
