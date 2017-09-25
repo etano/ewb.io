aae9b9b31fd4c0562a6d148570dd09b586e61203
Ethan Brown <ewb@ewb.io>
2017-09-25 22:11:49 +0200

    just use logs

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var ua = require("universal-analytics");
+var fs = require('fs')
 var express = require('express');
 var path = require('path');
+var morgan = require('morgan');
+var rfs = require('rotating-file-stream');
 
-var app = module.exports = express();
-var visitor = ua("UA-97838688-1");
+var app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.get('/', (req, res) => {
-   visitor.pageview(req.url).send();
-   res.render('index.html');
+// logs
+var log_directory = path.join(__dirname, 'log');
+if (!fs.existsSync(log_directory)) { fs.mkdirSync(log_directory); }
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: log_directory
 });
+
+// middleware
+app.use(morgan('combined', {stream: accessLogStream}))
+app.use(express.static(path.join(__dirname, '/public')));
+
+// routes
+app.get('/', (req, res) => res.render('index.html'));
+
+// run
 if(!module.parent) {
   var port = 1985; // Year of the Ox!
   app.listen(port, () => console.log('Running on port '+port));
