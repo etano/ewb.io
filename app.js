d2da2ca7591478fdfcc2be3d5e108b7ede65e40e
Ethan Brown <ewb@ewb.io>
2017-09-25 21:04:38 +0200

    added server side analytics

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var ua = require("universal-analytics");
 var express = require('express');
 var path = require('path');
 var app = module.exports = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.get('/', (req, res) => res.render('index.html'));
+app.use(ua.middleware("UA-97838688-1"));
+app.get('/', (req, res) => {
+   res.render('index.html');
+});
 if(!module.parent) {
   var port = 1985; // Year of the Ox!
   app.listen(port, () => console.log('Running on port '+port));
