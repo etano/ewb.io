77324d83085189c33dc143d392588ee571360648
Ethan Brown <ewb@ewb.io>
2020-09-25 22:56:44 +0200

    g2g

--- frontend/src/Navigation.svelte
+++ frontend/src/Navigation.svelte
@@ -1,24 +1,26 @@
 <style>
     #header {
-        max-width: 100%;
-        padding: 10px;
-        display: table;
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        margin: 20px;
+        padding: 10px 0 10px 10px;
+        display: flex;
         font-size: 14px;
         line-height: 24px;
         color: #999;
         background: white;
-        border: 2px solid black;
-        box-shadow: 5px 5px 5px black;
-        font-family: monospace;
-        position: absolute;
-        left: 20px;
-        top: 20px;
+        border: 2px solid #2F4858;
+        box-shadow: 3px 3px 5px #2F4858;
     }
     #name {
-        float: left;
+        padding: 0 10px 0 0;
+    }
+    #divider {
+        padding: 0 10px 0 0;
     }
     a:link, a:visited, a:active {
-        color: rgb(248, 117, 114);
+        color: #F97471;
         text-decoration: none;
     }
     a:hover {
@@ -28,20 +30,21 @@
         list-style-type: none;
         margin: 0;
         padding: 0;
-        float: left;
     }
     li {
-        padding: 0 0 0 10px;
+        padding: 0 10px 0 0;
+        float: left;
     }
 </style>
 
 <div id="header">
-    <div id="name">Ethan W. Brown ::</div>
+    <div id="name">Ethan W. Brown</div>
+    <div id="divider">::</div>
     <ul id="nav">
         <li><a href="https://github.com/etano">github</a></li>
         <li><a href="https://www.linkedin.com/pub/ethan-brown/45/a09/435">linkedin</a></li>
         <li><a href="https://scholar.google.ch/citations?user=sXEThvMAAAAJ&hl=en">scholar</a></li>
-        <li><a href="https://twitter.com/ethanwbrown">twitter</a>
+        <li><a href="https://twitter.com/ethanwbrown">twitter</a></li>
         <li><a href="https://keybase.io/ebrown">keybase</a></li>
     </ul>
 </div>
--- frontend/src/SketchBook.svelte
+++ frontend/src/SketchBook.svelte
@@ -1,17 +1,28 @@
 <script>
-    import Canvas from './Canvas.svelte'
+import Canvas from './Canvas.svelte'
+    let randomChoice = (arr) => {
+        return arr[Math.floor(Math.random() * arr.length)]
+    }
+
     let sketches = [
         (p5) => {
+            //let colors = ['#7180AC', '#2B4570', '#A8D0DB', '#E49273', '#A37A74']
+            //let colors = ['#EE7674', '#F9B5AC', '#D0D6B5', '#75B9BE', '#987284']
+            //let colors = ['#EE7674', '#CD688B', '#9E6394', '#6C5E8D', '#445576', '#2F4858']
+            //let colors = ['#FD9465', '#E3715B', '#F97471', '#E35B97', '#FD65F4']
+            //let colors = ['#FD9465', '#E77782', '#B86A94', '#7E6392', '#4B587B', '#2F4858']
+            let colors = ['#F97471', '#D6668C', '#A46298', '#6E5F91', '#435679', '#2F4858']
             p5.setup = () => {
                 p5.createCanvas(window.innerWidth, window.innerHeight)
             }
             p5.draw = () => {
-                p5.circle(p5.mouseX, p5.mouseY, p5.random(10, 80))
-                p5.fill(p5.random(255), p5.random(127), p5.random(51), 127)
+                p5.noStroke()
+                p5.fill(randomChoice(colors))
+                p5.circle(p5.mouseX, p5.mouseY, p5.random(5, 75))
             }
-        }
+        },
     ]
-    let sketch = sketches[Math.floor(Math.random() * sketches.length)]
+    let sketch = randomChoice(sketches)
 </script>
 
 <Canvas id="canvas" sketch={sketch}/>
